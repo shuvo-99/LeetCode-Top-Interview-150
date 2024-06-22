25f6d4568342fefa62955211eb7407c84f4a7cab
shuvo-99 <anontoshuvo99@gmail.com>
2024-06-23 01:06:09 +0600

    python

--- Array-String/27. Remove Element.py
+++ Array-String/27. Remove Element.py
@@ -11,3 +11,28 @@ class Solution:
         
 obj1 = Solution()
 obj1.removeElement([0,1,2,2,3,0,4,2], 2)
+
+# ================= Alternate ==============
+
+class Solution:
+    def removeElement(self, nums, val):
+        while val in nums:
+            nums.remove(val)
+        
+        print(nums)
+        
+obj1 = Solution()
+obj1.removeElement([3,2,2,3], 3)
+
+# ============ Better Approach ==========
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+        
+obj1 = Solution()
+obj1.removeElement([0,1,2,2,3,0,4,2], 2)
